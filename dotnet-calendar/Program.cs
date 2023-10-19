32ecb7a2126f32fb7817d77358cd5f9486992604
Tim Smith <tim@DESKTOP-MF3TCHA>
2023-10-19 15:59:28 +0100

    add tls termination to Docker .NET Core app

--- dotnet-calendar/Program.cs
+++ dotnet-calendar/Program.cs
@@ -1,22 +1,32 @@
+using System.Security.Claims;
+using Keycloak.AuthServices.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
+var services = builder.Services;
+var configuration = builder.Configuration;
+var host = builder.Host;
 
-// Add services to the container.
-
-builder.Services.AddControllersWithViews();
+services.AddKeycloakAuthentication(configuration);
+services.AddControllersWithViews();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
+//if (!app.Environment.IsDevelopment())
+//{
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+//    app.UseHsts();
+//}
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 
+app.MapGet("/", (ClaimsPrincipal user) => {
+    app.Logger.LogInformation(user?.Identity?.Name);
+}).RequireAuthorization();
 
 app.MapControllerRoute(
     name: "default",
